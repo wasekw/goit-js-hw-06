373534f224438ff1533589a7c7709207f1634999
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2021-10-08 21:59:27 +0300

    Some changes

---
+++ js/task-04.js
@@ -0,0 +1,11 @@
+// document.addEventListener("keydown", (event) => {
+//   console.log("Keydown: ", event);
+// });
+
+// document.addEventListener("keyup", (event) => {
+//   console.log("Keyup: ", event);
+// });
+document.addEventListener("keydown", (event) => {
+  // console.log("key: ", event.key);
+  console.log("code: ", event.code);
+});
