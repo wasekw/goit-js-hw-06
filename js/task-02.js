3b2763c4de4d791081cf898549d537639d4adda8
Vasiliy Volobaev <vasiliy.volobaev@gmail.com>
2023-02-13 16:05:58 +0200

    Second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach((el) => {
+  const item = document.createElement("li");
+  item.textContent = `${el}`;
+  list.append(item);
+});
